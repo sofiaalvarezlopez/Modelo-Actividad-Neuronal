6de3c591b275cd3a5ed230e2a1f313c17a8b0b15
Sofia Alvarez Lopez <ms.alvarezl@uniandes.edu.co>
2022-05-30 16:21:29 -0700

    Single file user interface

--- main.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-from vista.vistaPrincipal import ventana_principal
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    directorioActual = Path(__file__).parent
-    ventana_principal(directorioActual)
-
